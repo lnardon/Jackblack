a7e3ab4c689bb916beb0daa1500f1fe6c09a7cd3
lnardon <lucaslnardon@gmail.com>
2023-10-21 18:52:02 -0300

    ✨ Add cards draw functionality

--- frontend/src/lib/Cards.svelte
+++ frontend/src/lib/Cards.svelte
@@ -1,29 +1,46 @@
 <script>
-let rooms = [{id: 1, card: "2_of_clubs"}, {id: 2, card: "9_of_spades"}]
+    import {card_images} from "../cards"
+export let cards
 
+
+let rooms = [{id: 1, card: "2_of_clubs"}, {id: 2, card: "5_of_spades"}]
+setTimeout(() => {
+    rooms = [{id: 1, card: "2_of_clubs"}, {id: 2, card: "5_of_spades"},{id: 3, card: "10_of_diamonds"}]
+    setTimeout(() => {
+        rooms = [{id: 1, card: "2_of_clubs"}, {id: 2, card: "5_of_spades"},{id: 3, card: "10_of_diamonds"},{id: 3, card: "3_of_spades"}]
+    }, 6000)
+}, 4000)
 </script>
 
-<div>
-    {#each rooms as room}
-        <div>
-            <img src={"assets/cards/"+room.card+".png"} alt="Card: ">
+<div class="container">
+    {#each cards as card}
+        <div class="card_container">
+            <img src={"assets/cards/"+card_images[card]+".png"} alt="Card: ">
         </div>
     {/each}
 </div>
 
 <style>
-    div {
+    .container{
         display: flex;
-        justify-content: space-around;
+        justify-content: center;
+        align-items: center;
         flex-wrap: wrap;
+        gap: 2rem;
+        margin-bottom: 4rem;
+    }
+    .card_container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
         gap: 2rem;
     }
 
     img {
         width: 200px;
         height: 300px;
-        transition: all 0.9s cubic-bezier(0.075, 0.82, 0.165, 1);
-        animation: spin 1.1s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+        transition: all 0.5s cubic-bezier(0.075, 0.82, 0.165, 1);
+        animation: spin 0.5s cubic-bezier(0.175, 0.825, 0.64, 1.275);
         cursor: pointer;
     }
 
@@ -32,10 +49,12 @@ let rooms = [{id: 1, card: "2_of_clubs"}, {id: 2, card: "9_of_spades"}]
     }
 
     @keyframes spin {
-        0% { transform: rotateY(145deg); 
+        0% { 
+            transform: rotateY(90deg);
             opacity: 0;
         }
-        100% { transform: rotateY(0deg); 
+        100% { 
+            transform: rotateY(0deg);
             opacity: 1;
         }
     }
