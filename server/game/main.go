eb33bb8a9db3ce33ecad8e0afd428548c2da0850
lnardon <lucaslnardon@gmail.com>
2023-10-22 05:37:19 -0300

    ♻️ Update Game

--- server/game/main.go
+++ server/game/main.go
@@ -1,8 +1,32 @@
+package game
+
 import (
-  "fmt"
+	"fmt"
+	"math/rand"
 )
 
 type Game struct {
-  currentPlayer := 0
+  CurrentPlayer string
+  allCards [52]string
+  HasGameStarted bool
+  
 }
 
+func (g *Game)setCurrentPlayer(id string){
+  g.CurrentPlayer = id 
+}
+
+func (g *Game)GetRandomCard() (int) {
+  randNumber := rand.Intn(52)
+  return randNumber
+}
+
+func (g *Game)drawCard()int{
+	fmt.Println("Flong",g.CurrentPlayer)
+	card := g.GetRandomCard()
+	return card
+}
+
+func (g *Game)ping(){
+  fmt.Println("Flong",g.CurrentPlayer)
+}
